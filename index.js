2658751c9c260061e7533e0903ed382a9989532e
SuryanshShahi <suryansh06shahi@gmail.com>
2022-09-23 19:43:27 +0530

    stress therapist

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const changeBackground = () => {
   var x = document.getElementById("navbar");
-  var y = document.getElementById("topnav");
-  var z = document.getElementById("header");
   if (window.scrollY >= 100) {
-
     if (x.classList == "active") {
       x.classList.remove("active");
     } else {
       x.classList.add("active");
     }
-    y.style.display = "none";
-    z.style.backgroundColor = "transparent";
   } else {
     x.classList.remove("active");
-    y.style.display = "block";
-    z.style.backgroundColor = "white";
   }
 };
 window.addEventListener("scroll", changeBackground);
