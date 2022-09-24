1e6f08eb014e1a3fac93f01b2c3857ac9a6f0019
SuryanshShahi <suryansh06shahi@gmail.com>
2022-09-24 21:02:43 +0530

    stress therapist

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+const back = () => {
+  window.scroll(0, 0);
+};
+
 const changeBackground = () => {
   var x = document.getElementById("navbar");
+  var y = document.getElementById("backtotop");
   if (window.scrollY >= 100) {
     if (x.classList == "active") {
       x.classList.remove("active");
+      y.style.display ="none";
     } else {
       x.classList.add("active");
+      y.style.display ="block";
     }
   } else {
     x.classList.remove("active");
+    y.style.display ="none";
   }
 };
 window.addEventListener("scroll", changeBackground);
